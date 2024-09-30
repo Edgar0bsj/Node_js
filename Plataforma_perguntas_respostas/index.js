256e82d354553d1d65c408f2b4b9babe607854f1
Edgar barbosa da silva junior <edgarbsj.dev@gmail.com>
2024-09-30 13:05:31 -0300

    feat: condição

--- Plataforma_perguntas_respostas/index.js
+++ Plataforma_perguntas_respostas/index.js
@@ -6,11 +6,13 @@ app.set('view engine', 'ejs');
 app.get('/:nome/:leng', function(req, res) {
     let variavel_nome = req.params.nome;
     let variavel_leng = req.params.leng;
+    let variavel_admin = false;
     
     res.render('index',{
         nome: variavel_nome,
         lang: variavel_leng,
         testo: 'Qualquer coisa!',
+        admin: variavel_admin,
     });
 });
 
