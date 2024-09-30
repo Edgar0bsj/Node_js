63ac6630c6cef7104b38ebcec431b9f4a594f4b8
Edgar barbosa da silva junior <edgarbsj.dev@gmail.com>
2024-09-30 17:04:43 -0300

    feat: ForEach

--- Plataforma_perguntas_respostas/index.js
+++ Plataforma_perguntas_respostas/index.js
@@ -7,12 +7,23 @@ app.get('/:nome/:leng', function(req, res) {
     let variavel_nome = req.params.nome;
     let variavel_leng = req.params.leng;
     let variavel_admin = false;
+
+    let produto = [
+        {nome: 'Doritos', preco:5.60},
+        {nome: 'Carne', preco:60.00},
+        {nome: 'Nescau', preco:11.60},
+        {nome: 'ClubSocial', preco:3.50},
+        {nome: 'Vinagre', preco:4.60},
+
+    ];
+
     
     res.render('index',{
         nome: variavel_nome,
         lang: variavel_leng,
         testo: 'Qualquer coisa!',
         admin: variavel_admin,
+        produtos: produto,
     });
 });
 
